794a702e2007d0405bea8dc0d0b742d38155de95
Heiko Guckes <heiko.guckes@linked-planet.com>
2024-03-26 11:29:22 +0100

    fix GetIssueType Test

--- kotlin-jira-client/kotlin-jira-client-test-base/src/main/kotlin/com/linkedplanet/kotlinjiraclient/JiraIssueTypeOperatorTest.kt
+++ kotlin-jira-client/kotlin-jira-client-test-base/src/main/kotlin/com/linkedplanet/kotlinjiraclient/JiraIssueTypeOperatorTest.kt
@@ -40,10 +40,11 @@ interface JiraIssueTypeOperatorTest<JiraFieldType> : BaseTestConfigProvider<Jira
         val issueType = runBlocking { issueTypeOperator.getIssueType(issueTypeId) }.orFail()
         assertThat(issueType.id, equalTo(issueTypeId.toString()))
         assertThat(issueType.name, equalTo("Story"))
-        assertThat(issueType.iconUrl, equalTo("http://localhost"))
-        assertThat(issueType.description, equalTo(""))
-        assertThat(issueType.avatarId, equalTo(1337L))
-        assertThat(issueType.self, equalTo("http://localhost"))
+        assertThat(issueType.subTask, equalTo(false))
+        assertThat(issueType.description, containsString("Issue type for a user story."))
+        assertThat(issueType.self, endsWith("rest/api/2/issuetype/10001"))
+        assertThat(issueType.iconUrl, endsWith("images/icons/issuetypes/story.svg"))
+        assertThat(issueType.avatarId, equalTo(0L))
     }
 
     @Test
