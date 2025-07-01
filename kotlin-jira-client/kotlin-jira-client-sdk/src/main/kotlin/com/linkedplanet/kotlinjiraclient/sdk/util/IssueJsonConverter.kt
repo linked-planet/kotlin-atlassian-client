8ef1343877a6215e6376f1ed35ad04f0bd00f5a4
Heiko Guckes <heiko.guckes@linked-planet.com>
2025-07-01 11:20:16 +0200

    use Jackson for serialization, continue to use GSON for users of this client

--- kotlin-jira-client/kotlin-jira-client-sdk/src/main/kotlin/com/linkedplanet/kotlinjiraclient/sdk/util/IssueJsonConverter.kt
+++ kotlin-jira-client/kotlin-jira-client-sdk/src/main/kotlin/com/linkedplanet/kotlinjiraclient/sdk/util/IssueJsonConverter.kt
@@ -29,12 +29,14 @@ import com.atlassian.jira.issue.fields.rest.json.beans.JiraBaseUrls
 import com.atlassian.jira.rest.v2.issue.IncludedFields
 import com.atlassian.jira.rest.v2.issue.IssueBean
 import com.atlassian.jira.rest.v2.issue.builder.BeanBuilderFactory
-import com.google.gson.*
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser as GsonJsonParser
 import com.linkedplanet.kotlinjiraclient.api.model.IssueQueryParams
 import com.linkedplanet.kotlinjiraclient.sdk.field.FieldAccessorImpl
 import org.slf4j.LoggerFactory
 import javax.ws.rs.core.UriBuilder
-import javax.xml.bind.annotation.XmlTransient
 
 /**
  * Converts a Jira Issue to Json.
@@ -65,8 +67,6 @@ class IssueJsonConverter {
         }
     private val jiraBaseUrls: JiraBaseUrls = ComponentAccessor.getComponent(JiraBaseUrls::class.java)
     private val uriBuilder: UriBuilder = UriBuilder.fromPath(jiraBaseUrls.restApi2BaseUrl())
-    private val gson = setupGson()
-
 
     @Throws(FieldException::class)
     fun createJsonIssue(
@@ -80,7 +80,11 @@ class IssueJsonConverter {
         this.addOrderableFieldsToBean(issueBean, issue)
         this.addAvailableNavigableFieldsToBean(issueBean, issue)
 
-        return gson.toJsonTree(issueBean).asJsonObject
+        // Jackson is the official way now to serialize Beans. GSON will fail due to infinite loops in the model.
+        val jackson = jacksonObjectMapper()
+        val jacksonJson: JsonNode = jackson.valueToTree(issueBean)
+
+        return GsonJsonParser.parseString(jacksonJson.toString()).asJsonObject // expose as GSON for API compatibility
     }
 
     @Throws(FieldException::class)
@@ -111,17 +115,4 @@ class IssueJsonConverter {
             bean.addField(this, json, false)
         }
     }
-
-    private fun setupGson() =
-        GsonBuilder()
-            .setExclusionStrategies(object : ExclusionStrategy {
-                override fun shouldSkipField(f: FieldAttributes?): Boolean {
-                    return f?.getAnnotation(XmlTransient::class.java) != null
-                }
-
-                override fun shouldSkipClass(clazz: Class<*>?): Boolean {
-                    return false
-                }
-            })
-            .create()
 }
